a0e6bc8d75b588ce4a842817cdc712eea2a3c15a
Frederik Gebhardt <frederik.gebhardt@hotmail.de>
2025-03-11 17:37:11 +0100

    untrack notebook and implement data loading

--- ml_logic/data.py
+++ ml_logic/data.py
@@ -4,26 +4,42 @@ import numpy as np
 import ml_logic.params as params
 
 
-def load_full_data_set(data_dir):
+def load_full_data_set(data_dir, limit_num_img_per_class=10000):
     """
     This function loads the whole dataset and returns it in the format X, y
+    Loading the full dataset takes around 35 seconds.
     """
     file_names = load_all_filenames(data_dir)
-    return None
+
+    X = []
+    y = []
+    for k in file_names.keys():
+        for img_file_name in file_names[k][:limit_num_img_per_class]:
+            X.append(cv2.imread(os.path.join(data_dir, k, img_file_name)))
+        y.append(list(file_names.keys()).index(k))
+    X = np.asarray(X)
+    y = np.asarray(y)
+    return X, y
 
 def load_all_filenames(data_dir):
     # Get classes
-    class_list = os.listdir(params.DATA_DIR)
-    class_list.remove('.DS_Store')
-    print(f"Number of classes: {len(class_list)}")
+    class_folder_list = sorted(os.listdir(data_dir))
 
     # Get all file_names
     file_names = {}
-    for cl in class_list:
-        file_names[cl] = os.listdir(os.path.join(params.DATA_DIR, cl))
+    for folder in class_folder_list:
+        if os.path.isdir(os.path.join(data_dir, folder)):
+            # Check if folder name has 3 letters (like the class labels)
+            if len(folder) == 3:
+                file_list = os.listdir(os.path.join(data_dir, folder))
+                # Check if folder contains files
+                if len(file_list) > 0:
+                    file_names[folder] = file_list
     return file_names
 
 
 
 if __name__ == '__main__':
-    load_full_data_set(params.DATA_DIR)
+    X, y = load_full_data_set(params.DATA_DIR)
+    print(f"Shape X: {X.shape}")
+    print(f"Shape y: {y.shape}")
