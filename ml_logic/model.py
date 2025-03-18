1fd00bd95e29633d7691f24de1faa972ff40cc49
Anton Schwericke <schwericke@mail.de>
2025-03-18 14:45:47 +0100

    ConvNeXtXLarge

--- ml_logic/model.py
+++ ml_logic/model.py
@@ -3,16 +3,20 @@ from tensorflow.keras import models, layers
 
 
 def create_compile_model_fredi():
-    base_model = tf.keras.applications.ResNet50(
+    base_model = tf.keras.applications.ConvNeXtXLarge(
         include_top=False,
+        include_preprocessing=True,
         weights="imagenet",
-        input_shape=(224, 224, 3),
+        input_shape=(384, 384, 3),
     )
     model = models.Sequential([
-        base_model,                                        # Add the ResNet50 base model
+        base_model,                                         # Add the base model
         layers.GlobalAveragePooling2D(),                    # Add pooling layer to reduce feature map size
-        layers.Dense(1024, activation='relu'),              # Add a fully connected hidden layer (optional)
-        layers.Dropout(0.5),                                # Add a dropout layer to reduce overfitting
+        layers.Dense(1024, activation='relu'),              # Add a fully connected hidden layer
+        layers.BatchNormalization(),
+        layers.Dropout(0.3),
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
         layers.Dense(15, activation='softmax')              # Custom output layer (e.g., for 10 classes, use softmax for multi-class classification)
     ])
 
