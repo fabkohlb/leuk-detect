3eaea2b677b598b5c466fde67d5a4466260e37ca
Frederik Gebhardt <frederik.gebhardt@hotmail.de>
2025-03-19 14:20:20 +0100

    bug fix

--- ml_logic/registry.py
+++ ml_logic/registry.py
@@ -6,7 +6,7 @@ print(f"Current working directory: {os.getcwd()}")
 from tensorflow import keras
 from google.cloud import storage
 import os
-from ml_logic import params
+import params
 import time
 
 import mlflow
